cf2303b07a8136206986763cbad64c779479a322
rileyellwanger <rileyellwanger@gmail.com>
2022-03-13 12:03:48 -0500

    init about me content

---
+++ src/content/intro.js
@@ -0,0 +1,12 @@
+const intro = <p>I grew up in Nebraska where it's all about the Cornhuskers. So going to the university 
+    (for football games, and for school) was always a dream. I received the Regent's Scholarship and 
+    originally majored in Wildlife Ecology because of my love for the outdoors. I've been  
+    hunting and fishing my entire life, and wanted to pursue a career conserving wildlife and natural 
+    resources so future generations can enjoy the same opportunities I have. 
+    <br/>
+    I made the jump to this evergrowing industry of software for the endless possibilities of opportunities 
+    where I will be challenged continuously, creatively solve problems, and just build cool stuff! I hope to 
+    contribute to conservation with my software development skills in the future. 
+</p>
+
+export default intro
