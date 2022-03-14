c53f150be548a81db955774fc572e87a9a944691
rileyellwanger <rileyellwanger@gmail.com>
2022-03-13 20:15:43 -0500

    Update bio.js

--- src/content/bio.js
+++ src/content/bio.js
@@ -1,4 +1,4 @@
-const bio = <p>I'm an ex-Wildlife Ecology major, who made a huge transition to 
+const bio = <p>I was a Wildlife Ecology major, but I made a huge transition to 
     Computer Science right before my senior year at the University of Nebraska - Lincoln. <br/>
     Now I'm a full-stack software developer and consultant, who loves understanding and 
     building complex systems. </p>
