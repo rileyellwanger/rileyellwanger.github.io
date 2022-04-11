eca9b513d3cb98c738fe068add539a04a1de849f
rileyellwanger <rileyellwanger@gmail.com>
2022-04-11 15:34:07 -0500

    content revision

--- src/content/bio.js
+++ src/content/bio.js
@@ -3,8 +3,8 @@ const bio =
     I was a Wildlife Ecology major working on becoming a wildlife biologist, but my senior year I made
     a huge switch to Computer Science.
     <br />
-    Now I'm a full-stack software developer and consultant, who loves
-    understanding and building complex systems.
+    Now I'm a full-stack software developer, who loves
+    building and learning complex systems.
   </p>
 
 export default bio;
