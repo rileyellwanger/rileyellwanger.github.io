1e461382fb8b7ada5c1364985d4d4e1886055588
Riley Ellwanger <riley.ellwanger@sentinelfertigation.com>
2025-02-20 12:50:22 -0600

    purge

--- src/content/welcome.js
+++
@@ -1,9 +0,0 @@
-const welcome = <p>Welcome to my personal website and portfolio, and thanks for stopping by.
-    Here you'll find info about myself, and my software development experience. <br/>
-    I created this page as a living resume where I can share my professional experience, 
-    as well as personal interests. <br/>
-    This is a work in progress, and I'll be updating as often as I can. Feel free to reach 
-    out with feedback.
-</p>
-
-export default welcome
