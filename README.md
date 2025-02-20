7999ad29fd2f56cf28923e2643217f561ac752a8
Riley Ellwanger <riley.ellwanger@sentinelfertigation.com>
2025-02-20 12:56:44 -0600

    v2

---
+++ README.md
@@ -0,0 +1,25 @@
+# Portfolio
+
+## Technologies Used
+
+- [Next.js 14](https://nextjs.org/docs/getting-started)
+- [HeroUI](https://heroui.com)
+- [Tailwind CSS](https://tailwindcss.com)
+- [Tailwind Variants](https://tailwind-variants.org)
+- [TypeScript](https://www.typescriptlang.org)
+- [Framer Motion](https://www.framer.com/motion)
+- [next-themes](https://github.com/pacocoursey/next-themes)
+
+### Install dependencies
+
+You can use one of them `npm`, `yarn`, `pnpm`, `bun`, Example using `npm`:
+
+```bash
+npm install
+```
+
+### Run the development server
+
+```bash
+npm run dev
+```
---
+++ pages/index.tsx
@@ -0,0 +1,104 @@
+import { Card } from "@heroui/card";
+import { Image } from "@heroui/image";
+import { Spacer } from "@heroui/spacer";
+
+import { title, subtitle } from "@/components/primitives";
+import DefaultLayout from "@/layouts/default";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <Card className="w-full h-[550px] relative overflow-hidden">
+        <div className="absolute inset-0">
+          <Image
+            alt="Background"
+            className="object-cover"
+            height={600}
+            src="/digital tree background.jpg"
+            width={1400}
+          />
+        </div>
+        <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center bg-black/50">
+          <div className={title({ class: "text-white my-4 " })}>
+            Bridging the gap between the ground and the cloud
+          </div>
+          <div className={subtitle({ class: "text-white px-20" })}>
+            Welcome to my portfolio, I&apos;m Riley, a software engineer with my
+            hands in the dirt and my head in the clouds. Here, I blend my
+            passion for agriculture and the outdoors with cutting-edge
+            technology to tackle complex challenges with elegantly simple
+            solutions.
+          </div>
+        </div>
+      </Card>
+
+      <Spacer y={16} />
+
+      <Card
+        isPressable
+        className="w-full h-[450px] relative overflow-hidden"
+        onPress={() => {
+          window.location.href = "/about";
+        }}
+      >
+        <div className="absolute inset-0">
+          <Image
+            alt="Background"
+            className="object-cover"
+            height={600}
+            src="/tractor background.jpg"
+            width={1400}
+          />
+        </div>
+        <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center bg-black/50">
+          <div className={title({ class: "text-white my-4 " })}>About</div>
+          <div className="text-white text-lg px-20">
+            Growing up on a farm, I learned to appreciate hard work, patience,
+            and nature while riding in the combine with my grandpa and working
+            on a cattle ranch with my dad. This, along with my love for hunting
+            and fishing, lead me to initially pursue a degree in Wildlife
+            Ecology, but once I realized the potential of software to amplify my
+            impact on both agriculture and conservation, I switched to Computer
+            Science. Now, my on the ground experience fuels my approach to
+            software design, ensuring it&apos;s efficient and sustainable for
+            the people who use it.
+          </div>
+        </div>
+      </Card>
+
+      <Spacer y={16} />
+
+      <Card
+        isPressable
+        className="w-full h-[450px] relative overflow-hidden"
+        onPress={() => {
+          window.location.href = "/work";
+        }}
+      >
+        <div className="absolute inset-0">
+          <Image
+            alt="Background"
+            className="object-cover"
+            height={600}
+            src="/corn background.jpg"
+            width={1400}
+          />
+        </div>
+        <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center bg-black/50">
+          <div className={title({ class: "text-white my-4 " })}>Work</div>
+          <div className="text-white text-lg px-20">
+            I have successfully merged my passion for agriculture with my work
+            at Sentinel Fertigation, an AgTech startup, where I develope
+            innovative sotware solutions that help farmers optimize their
+            nitrogen management; reducing their environmental impact, and
+            increasing their profitability. I have gained a wealth of experience
+            in full-stack development, geospatial data analysis, and image
+            processing, and I am always looking for new ways to leverage
+            technology to improve the world around me.
+          </div>
+        </div>
+      </Card>
+      <Spacer y={16} />
+    </DefaultLayout>
+  );
+}
